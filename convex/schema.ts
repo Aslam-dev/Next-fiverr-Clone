53d785a2892a47f58e020752a5ed1d984258f3fa
Aslam-dev <almaslam01@gmail.com>
2024-05-14 10:32:24 +0530

    complete schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,5 +1,7 @@
 import < v > from "convex/values";
 import {defineSchema, defineTable } from "convex/server";
+import { format } from "path";
+import { getgid } from "process";
 
 export default defineSchema({
     users: defineTable({
@@ -16,5 +18,119 @@ export default defineSchema({
         stripeAccountSetupComplete: v.optional(v.boolean()),
     })
 
-
+           .index("by_token", ["tokenIdentifier"])
+            .index("by_username", ["username"])
+        reviews: defineTable({
+            autherId: v.id("users"),
+            sellerId: v.id("users"),
+            gigId: v.id("gigs"), 
+            comment: v.string(),
+            communication_level: v.number(),
+            recommend_to_a_friend: v.number(),
+            service_as_described: v.number(),
+        })
+            .index("by_sellerId", ["sellerId"])
+            .index("by_gigId", ["gigId"]),
+        skills: defineTable({
+             skill: v.string(),
+             userId: v.id("users"),
+        })
+            .index("by_uderId",["userId"]),
+        languages: defineTable({
+            language: v.string(),
+            userId: v.id("user"),
+        })
+            .index("by_userId",["userId"]),
+        userFlags: defineTable ({
+            userId: v.id("users"),
+            markingType: v.string(),
+            description: v.string(),
+        }),
+        countries: defineTable({
+            countryName: v.string(),
+            userId: v.id("user"),
+        })
+            .index("by_userId",["userId"]),
+        gigs: defineTable({
+            title: v.string(),
+            description: v.string(),
+            sellerId: v.id("user"),
+            subcategoryId: v.id("subcategoryies"),
+            published: v.optional(v.boolean()),
+            clicks: v.number(),
+        })  
+            .index("by_sellerId",["sellerId"])
+            .index("by_subcategoryId",["subcategoryId"])
+            .index("by_published",["published"])
+            .searchIndex("search_title",{
+                searchField:"title",
+            }),
+        offers: defineTable({
+            gigId: v.id("gigs"),
+            title: v.string(),
+            description: v.string(),
+            tier: v.union(
+                v.literal("Basic"),
+                v.literal("Standerd"),
+                v.literal("Premium")
+            ),
+            price: v.number(),
+            delivery_days: v.number(),
+            revisions: v.number(),
+            stripePriceId: v.string(),
+        })
+            .index("by_gigId", ["gigId"])
+            .index("by_tier", ["tier"])
+            .index("by_gigId_tier",["gigId","tier"]),
+        orders: defineTable({
+            offerId: v.id("offers"),
+            gigId: v.id("gigs"),
+            buyersId: v.id("users"),
+            fulfillmentStatus: v.string(),
+            fulfilmentTime: v.optional(v.number()),
+        })
+            .index("by_buyerId", ["buyerId"])
+            .index("by_gigId", ["gigId"]),
+        gigMedia: defineTable({
+            storageId: v.id("_storage"),
+            format: v.string(),
+            gigId: v.id("gigs"),
+        })
+            .index("by_gigId", ["gigId"])
+            .index("by_storageId", ["storageId"]),
+        categories: defineTable({
+            name: v.string(),
+        }),
+        subcategories: defineTable({
+            categoryId: v.id("categories"),
+            name: v.string(),
+        })
+            .index("by_category",["categoryId"])
+            .index("by_name",["name"]),
+        faq: defineTable({
+            questine: v.string(),
+            answer: v.string(),
+            gigId: v.id("gigs"),
+        }),
+        messages: defineTable({
+            userId: v.id("users"),
+            text: v.optional(v.string()),
+            imageUrl: v.optional(v.string()),
+            seen: v.boolean(),
+            conversationId: v.id("conversation"),
+        })
+            .index('by_conversationId',['conversationId']),
+        conversations: defineTable({
+            participantOneId: v.id("users"),
+            participantTwoId: v.id("users"),
+        })
+            .index('by_participantOneId',['participantOneId','participantTwoId'])
+            .index('by_participantTwoId',['participantTwoId','participantOneId']),
+        userFavorites: defineTable({
+            userId:v.id("user"),
+            getgId: v.id("gigs"),
+        })
+            .index("by_gig",["gigId"])
+            .index("by_user_gig", ["userId","gigId"])
+            .index("by_user",["userId"]),
 });
